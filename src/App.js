4d5556df4d485239ef197cdca3c1653b8ba53f52
ravalibarma <156193231+ravalibarma@users.noreply.github.com>
2024-03-23 17:11:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
     <h1>akhi</h1>
     <img src="https://images.pexels.com/photos/462118/pexels-photo-462118.jpeg?cs=srgb&dl=bloom-blooming-blossom-462118.jpg&fm=jpg"></img>
    <img src="https://www.bing.com/th?id=OIP.3MxqaJv2Z5QsG7wIXzizjAHaEo&w=159&h=104&c=8&rs=1&qlt=90&o=6&dpr=1.5&pid=3.1&rm=2">
+    <img src="https://th.bing.com/th?id=OIP.RG58DaO7kXIPA2oTSLS6kAHaE8&w=306&h=204&c=8&rs=1&qlt=90&o=6&dpr=1.5&pid=3.1&rm=2"></img>
     
     
     </img> </div>
